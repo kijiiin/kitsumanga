e3bef33312b492e3dc114ba338452cc8195845bb
kijiiin <angelopace39@gmail.com>
2026-04-14 19:33:36 +0200

    .

--- js/sw.js
+++ js/sw.js
@@ -2,12 +2,26 @@ const CACHE_NAME = "kitsu-cache-v1";
 
 const urlsToCache = [
   "./",
-  "./index.html"
+  "./index.html",
+  '/pages/manga-detail.html',
+  '/pages/external-reader.html',
+  '/data/manga.json',
+  '/data/external.json'
 ];
 
 self.addEventListener("install", e => {
   e.waitUntil(
-    caches.open(CACHE_NAME).then(cache => cache.addAll(urlsToCache))
+    caches.open(CACHE_NAME)
+    .then(cache => {
+      // Usa cache.addAll() solo se tutti i file esistono
+        return cache.addAll(urlsToCache).catch(err => {
+          console.log('Cache addAll failed:', err);
+          // Prova a fare cache uno per uno
+          return Promise.allSettled(
+            urlsToCache.map(url => cache.add(url).catch(e => console.log('Failed:', url)))
+          );
+          });
+        })
   );
 });
 
